2b40034a57f4c498afa9442d32906293db480b70
code-das <bibek.das.kol@gmail.com>
2021-09-15 13:17:22 +0530

    Updates

---
+++ precache-manifest.bb8d5eb2de489edb9aeb942cdfbb2c52.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707f2f447ed3b2538f516591c9036075",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b2897d30d0f497562ddf",
+    "url": "/portfolio/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "ed3e96f54d89595dba12",
+    "url": "/portfolio/static/js/2.f8ef4d6c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio/static/js/2.f8ef4d6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2897d30d0f497562ddf",
+    "url": "/portfolio/static/js/main.bf417819.chunk.js"
+  },
+  {
+    "revision": "c3fe483896071cc46d75",
+    "url": "/portfolio/static/js/runtime-main.75c69042.js"
+  },
+  {
+    "revision": "86e7b0fa47974d89a069f1ac779b2631",
+    "url": "/portfolio/static/media/DSC_0285.86e7b0fa.jpg"
+  },
+  {
+    "revision": "8087b38c3c19af3fd21fc7cdb4f95913",
+    "url": "/portfolio/static/media/ResumeBibekDas.8087b38c.pdf"
+  },
+  {
+    "revision": "80399d607ffa8be7e1f5e283ae76151e",
+    "url": "/portfolio/static/media/bibekdas.80399d60.jpg"
+  },
+  {
+    "revision": "8f56e794206521b985a14502a5967333",
+    "url": "/portfolio/static/media/highradius.8f56e794.png"
+  }
+]);
